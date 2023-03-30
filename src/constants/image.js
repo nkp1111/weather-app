6e615a691159255058eb093e98953e31af302c23
nkp1111 <neerajp4321@gmail.com>
2023-03-30 16:38:33 +0530

    feat: add constant file to export all asset images

---
+++ src/constants/image.js
@@ -0,0 +1,23 @@
+import Clear from '../assets/Clear.png'
+import CloudBackground from '../assets/Cloud-background.png'
+import Hail from '../assets/Hail.png'
+import HeavyCloud from '../assets/HeavyCloud.png'
+import LightCloud from '../assets/LightCloud.png'
+import LightRain from '../assets/LightRain.png'
+import Shower from '../assets/Shower.png'
+import Sleet from '../assets/Sleet.png'
+import Snow from '../assets/Snow.png'
+import Thunderstorm from '../assets/Thunderstorm.png'
+
+export default {
+  Clear,
+  CloudBackground,
+  Hail,
+  HeavyCloud,
+  LightCloud,
+  LightRain,
+  Shower,
+  Sleet,
+  Snow,
+  Thunderstorm
+}
